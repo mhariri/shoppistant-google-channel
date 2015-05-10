6fed2efc5a56ee5e3245aac03e9b03fd7ca4ab87
Mohsen <mh@mohsen-macbook.mhariri.info>
2015-05-10 20:35:27 +0200

    - fix caching

--- main.py
+++ main.py
@@ -44,6 +44,7 @@ class MainHandler(webapp2.RequestHandler):
                 if open_details:
                     self.redirect(str("http://www.google.se/search?q=%s" % urllib.quote_plus(name)))
                 else:
+                    self.set_default_headers()
                     self.response.content_type = "image/png"
                     self.response.body_file = open("icon.png")
             except urllib2.HTTPError, e:
